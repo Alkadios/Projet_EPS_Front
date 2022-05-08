1ae45957858bb88448549804f61b343c46994d0e
Quentin <quentin-ol@hotmail.com>
2022-05-08 11:08:37 +0200

    feat: PWA [WIP]

--- vue.config.js
+++ vue.config.js
@@ -3,4 +3,25 @@ module.exports = {
     port: 8080,
   },
   publicPath: '/',
+  pwa: {
+    nom: 'OCPEPS',
+    themeColor: '#4e73df',
+    icônes: {
+      favicon32: 'assets/icons/favicon-32x32.png',
+      favicon16: 'assets/icons/favicon-16x16.png',
+      appleTouchIcon: 'assets/icons/apple-touch-icon-152x152.png',
+      maskIcon: 'assets/icons/safari-pinned-tab.svg',
+      msTileImage: 'assets/icons/msapplication-icon-144x144.png',
+    },
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+
+    // configure the workbox plugin
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      swSrc: 'dev/sw.js',
+      // ...other Workbox options...
+    },
+  },
 };
