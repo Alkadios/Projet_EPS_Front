04e8179eb1369162376710e22c25909d4d614c82
Quentin <quentin-ol@hotmail.com>
2022-05-11 16:44:12 +0200

    Change API Variable

--- src/constants/ApiUrls.ts
+++ src/constants/ApiUrls.ts
@@ -1,10 +1,11 @@
+export const TA_GROSSE_DARONNE = 'https://ocpeps.widenet.fr/api';
 export const ApiUrl = 'https://opceps.widenet.fr/api';
 export const ApiUrlUtilisateur = `${ApiUrl}/utilisateur`;
 export const ApiUrlChampApprentissage = `${ApiUrl}/champ_apprentissages`;
 export const ApiURLAPSA = `${ApiUrl}/apsas`;
 export const ApiURLAf = `${ApiUrl}/afs`;
 export const ApiURLAfRetenus = `${ApiUrl}/af_retenus`;
-export const ApiURLApsaSelect = `${ApiUrl}/apsa_select_annees`;
+export const ApiURLApsaSelect = `${TA_GROSSE_DARONNE}/apsa_select_annees`;
 export const ApiURLNiveauScolaire = `${ApiUrl}/niveau_scolaires`;
 export const ApiURLChoixAnnee = `${ApiUrl}/choix_annees`;
 export const ApsaRetenuAPI = `${ApiUrl}/apsa_retenus`;
